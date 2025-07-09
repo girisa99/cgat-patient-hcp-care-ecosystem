2f9af5747c517b780ddf9f9d65b13390f2e5c34f
Saigiridhar Dasika <dasikasaigiridhar@gmail.com>
2025-07-09 11:17:16 -0400

    chore(dev): dev server & tunnel on port 8080

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,16 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
+<<<<<<< HEAD
     host: "::",
     port: 8080,
     // Hosts allowed to access the dev server
     allowedHosts: ["localhost", "dev.geniecellgene.com"],
+=======
+  host: "0.0.0.0",
+  port: 8080,                               
+  allowedHosts: ["localhost", "dev.geniecellgene.com"],
+>>>>>>> 0f86293 (chore(dev): dev server & tunnel on port 8080)
   },
   plugins: [
     react(),
